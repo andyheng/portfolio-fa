0bea7dd81c9d3d62bf2d531b4afa7d612eb23efe
Andy <andy.dot.heng@gmail.com>
2019-02-18 12:14:04 -0500

    commit 404 page template

---
+++ src/components/NotFound.vue
@@ -0,0 +1,34 @@
+<template>
+    <main>
+        <div class="container container-404">
+            <img src="../assets/img/collar.gif" alt="simpsons awkward collar pull gif" v-if="!mobile">
+            <img src="../assets/img/collar.jpg" alt="simpsons awkward collar pull jpg" v-else class="img-404-jpg">
+            <h1>404</h1>
+            <p>Sorry, I couldn't find the page you were looking for. Try one of these pages:</p>
+            <ul class="list-404">
+                <li>
+                    <router-link to="/" class="link-404">Home</router-link>
+                </li>
+                <li>
+                    <router-link to="/contact" class="link-404">Contact</router-link>
+                </li>
+                <li>
+                    <router-link to="/extra" class="link-404">Extra</router-link>
+                </li>
+            </ul>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mobile: ""
+        };
+    },
+    created() {
+        this.mobile = window.innerWidth <= 600;
+    }
+}
+</script>
