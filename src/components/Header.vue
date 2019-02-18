a230a279b989f33ca58b899a27ab829af5e13ba3
Andy <andy.dot.heng@gmail.com>
2019-02-18 11:51:52 -0500

    implement auth conditional rendering

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,32 @@
 <template>
   <header ref="vueHeaderRef">
-    <div class="container header-container">
+    <div class="container header-container header-container-border" ref="vueHeaderContainerRef">
       <div class="header-title">
         <div class="header-logo">
-          <a href="#">
-            <img src="../assets/logo-rs.png">
-          </a>
+          <router-link to="/">
+            <img src="../assets/logo-rs.png" @click="toggleNav">
+          </router-link>
         </div>
         <div class="header-burger" :class="{'header-burger-open':showNav}" @click="toggleNav">
-          <font-awesome-icon icon="bars" />
+          <font-awesome-icon icon="bars"/>
         </div>
       </div>
       <nav class="nav" v-show="showNav">
         <ul class="nav-list">
+          <li class="nav-item" v-if="loggedInAdmin">
+            <router-link to="/admin" class="nav-link">Admin</router-link>
+          </li>
+          <li class="nav-item" v-if="loggedInAdmin">
+            <span class="nav-link" @click="handleLogout">Log out</span>
+          </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <router-link to="/extra" class="nav-link">Extra</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" class="nav-link">Contact</router-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Instagram</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Extra</a>
           </li>
         </ul>
       </nav>
@@ -29,18 +35,57 @@
 </template>
 
 <script>
+import { auth } from "../main";
 export default {
   data() {
     return {
-      showNav: false
+      showNav: false,
+      loggedInAdmin: false
     };
   },
   methods: {
     toggleNav() {
       this.showNav = !this.showNav;
+    },
+    handleLogout() {
+      auth.signOut();
+      auth.onAuthStateChanged(() => {
+        this.loggedInAdmin = false;
+      });
+      this.$router.push("/");
+    },
+    checkAuth() {
+      auth.onAuthStateChanged(user => {
+        if (user) {
+          user.getIdTokenResult()
+            .then(idTokenResult => {
+              user.admin = idTokenResult.claims.admin
+              this.loggedInAdmin = true;
+            })
+        } else {
+          this.loggedInAdmin = false;
+        }
+      })
+    },
+    showHeaderBorder() {
+      if (this.$route.name !== "Home") {
+        this.$refs.vueHeaderContainerRef.classList.add("header-container-border-hidden");
+      } else {
+        this.$refs.vueHeaderContainerRef.classList.remove("header-container-border-hidden");
+      }
+    }
+  },
+  watch: {
+    $route() {
+      this.showNav = false;
+      this.checkAuth();
+      this.showHeaderBorder();
     }
   },
   created() {
+    this.checkAuth();
+  },
+  mounted() {
     document.addEventListener("scroll", () => {
       const scroll = window.scrollY;
       const header = this.$refs.vueHeaderRef;
@@ -50,6 +95,7 @@ export default {
         header.classList.remove("header-scroll-active");
       }
     });
+    this.showHeaderBorder()
   }
 };
 </script>
