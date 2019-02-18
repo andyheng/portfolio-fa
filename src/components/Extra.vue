db9045ce7e5e40dd6db4575ce682fbcec747bb1c
Andy <andy.dot.heng@gmail.com>
2019-02-18 13:56:51 -0500

    update dist

---
+++ src/components/Extra.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="container">
+            test page
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
