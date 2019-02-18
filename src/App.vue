4c1cf6fe1593b0a4f82de0804947641e00ec5057
Andy <andy.dot.heng@gmail.com>
2019-02-18 11:49:43 -0500

    implement firebase call and router transition

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,57 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <portfolio-items></portfolio-items>
+    <transition name="fade">
+      <div class="loading" v-show="portfolioItems.length === 0 && this.$route.name === 'Home'"></div>
+    </transition>
+    <transition name="fade" mode="out-in" @beforeLeave="beforeLeave" @enter="enter" @afterEnter="afterEnter">
+        <router-view :itemsProp="portfolioItems"></router-view>
+    </transition>
     <v-footer></v-footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
-import PortfolioItems from "./components/PortfolioItems";
 import Footer from "./components/Footer";
+import { db } from "./main";
 
 export default {
   name: 'app',
   components: {
     "v-header": Header,
-    "portfolio-items": PortfolioItems,
     "v-footer": Footer
   },
+  data() {
+    return {
+      portfolioItems: [],
+      prevHeight: 0
+    }
+  },
+  methods: {
+    // before we transition to a new page...
+    beforeLeave(e) {
+      // getComputedStyle returns an object containing ALL CSS values / properties
+      // here we get the height of the app before we move to another page
+      this.prevHeight = getComputedStyle(e).height;
+    },
+    // when we enter a new page...
+    enter(e) {
+      const { height } = getComputedStyle(e);
+      e.style.height = this.prevHeight;
+      setTimeout(() => {
+        e.style.height = height;
+      });
+    },
+    afterEnter(e) {
+      e.style.height = "auto";
+    }
+  },
+  firestore() {
+    return {
+      portfolioItems: db.collection("projects")
+    }
+  }
 }
 </script>
 
