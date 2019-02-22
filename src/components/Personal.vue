4adb00656c029e5c66b06dcd8fae1836203a7d34
Andy <andy.dot.heng@gmail.com>
2019-02-22 13:59:42 -0500

    commit personal component

---
+++ src/components/Personal.vue
@@ -0,0 +1,83 @@
+<template>
+  <main>
+      <div class="container portfolio-container">
+      <div class="portfolio-item" v-for="personalItem in personalItems" :key="personalItem.id">
+        <img
+          class="portfolio-img"
+          @load="fade"
+          @click="showModalSetData(personalItem)"
+          v-lazy="personalItem.i[0].src"
+        >
+      </div>
+    </div>
+    <modal @close="closeModal" :show="showModal" :item="currentItem" :mobile="mobile"></modal>
+  </main>
+</template>
+
+<script>
+import Modal from "./Modal";
+import createHistory from "history/createBrowserHistory";
+const history = createHistory();
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      personalItems: [],
+      currentItem: {},
+      showModal: false,
+      addBodyClass: true,
+      bodyClassName: "modal-open",
+      mobile: this.setMobileBoolean()
+    };
+  },
+  props: ["personalProp"],
+  methods: {
+    fade(e) {
+      let target = e.target;
+      setTimeout(() => {
+        target.classList.add("portfolio-img-fadein")
+      }, 150);
+    },
+    closeModal() {
+      history.replace("/");
+      this.toggleBodyModalOpenClass(!this.addBodyClass);
+      this.showModal = false;
+    },
+    toggleBodyModalOpenClass(bodyClassBool) {
+      const el = document.body;
+      if (bodyClassBool) {
+        el.classList.add(this.bodyClassName);
+      } else {
+        el.classList.remove(this.bodyClassName);
+      }
+    },
+    showModalSetData(portfolioItem) {
+      const formatTitle = portfolioItem.t.replace(/ /g, "-").toLowerCase();
+      history.push(`/${formatTitle}`);
+      this.currentItem = portfolioItem;
+      this.showModal = true;
+      this.toggleBodyModalOpenClass(this.addBodyClass);
+    },
+    setMobileBoolean() {
+      const mobileMaxWidth = 600;
+      return innerWidth <= mobileMaxWidth;
+    }
+  },
+  created() {
+    this.personalItems = this.personalProp;
+
+    addEventListener("resize", () => {
+      this.mobile = this.setMobileBoolean();
+    });
+
+    history.listen((location, action) => {
+      if (action == "POP") {
+        this.closeModal();
+      }
+    });
+  }
+};
+</script>
