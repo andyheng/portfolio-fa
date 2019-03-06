22694015fecbd3f453d82fb5acd7ba8e4cf86789
Andy <andy.dot.heng@gmail.com>
2019-03-05 20:35:55 -0500

    use lazy route loading

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,23 @@
-import PortfolioItems from "./components/PortfolioItems";
-import Contact from "./components/Contact";
-import Personal from "./components/Personal";
+// import PortfolioItems from "./components/PortfolioItems";
+// import Contact from "./components/Contact";
+// import Personal from "./components/Personal";
 import Admin from "./components/Admin";
 import Login from "./components/Login";
-import NotFound from "./components/NotFound";
+// import NotFound from "./components/NotFound";
 
 import { auth } from "./main";
 const title = "Fahdad Atin";
+const loadView = view => {
+  return () => import(`@/components/${view}.vue`)
+}
 export default [
-  {path: "/", component: PortfolioItems, props: true, name: "Home", meta: {title}},
-  {path: "/contact", component: Contact, name: "Contact", meta: {title}},
-  {path: "/personal", component: Personal, name: "Personal", meta: {title}},
-  {path: "*", component: NotFound, name: "404", meta: {title}},
+  {path: "/", component: loadView("PortfolioItems"), props: true, name: "Home", meta: {title}},
+  {path: "/contact", component: loadView("Contact"), name: "Contact", meta: {title}},
+  {path: "/personal", component: loadView("Personal"), name: "Personal", meta: {title}},
+  {path: "*", component: loadView("NotFound"), name: "404", meta: {title}},
   {
     path: "/admin", 
-    component: Admin, 
+    component: loadView("Admin"), 
     meta: {requireAuth: true, title},
     beforeEnter: (to, from, next) => {
       const requireAuth = to.matched.some(record => record.meta.requireAuth);
@@ -27,5 +30,5 @@ export default [
       })
     }
   },
-  {path: "/login", component: Login, meta: {title}}
+  {path: "/login", component: loadView("Login"), meta: {title}}
 ]
