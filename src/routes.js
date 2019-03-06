9388acb38b45ff1d2f67200bc693aeaa73e2a7e6
Andy <andy.dot.heng@gmail.com>
2019-03-06 14:48:28 -0500

    add rerouting when page is refreshed with modal open

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { auth } from "./main";
+import createHistory from "history/createBrowserHistory";
+const history = createHistory();
 const title = "Fahdad Atin";
 const loadView = view => {
   return () => import(`@/components/${view}.vue`)
 }
 export default [
-  {path: "/", component: loadView("PortfolioItems"), props: true, name: "Home", meta: {title}},
+  {path: "/", component: loadView("PortfolioItems"), props: true, name: "Home", meta: {title}, beforeEnter: (to, from, next) => {
+    history.replace("/");
+    next()
+  }},
   {path: "/contact", component: loadView("Contact"), name: "Contact", meta: {title}},
   {path: "/personal", component: loadView("Personal"), name: "Personal", meta: {title}},
+  {path: "/personal/*", redirect: "/personal"},
   {path: "*", component: loadView("NotFound"), name: "404", meta: {title}},
   {
     path: "/admin", 
