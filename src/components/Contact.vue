68f52233f66e0fafb88cbabeb6748356e7dea507
Andy <andy.dot.heng@gmail.com>
2019-09-19 12:41:26 -0400

    update links, assets, styles (rewritten)

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,64 +1,48 @@
 <template>
-  <main>
-    <div class="contact-page-container">
-      <div class="contact-header contact-container">
-        <div class="contact-header-inside">
-          <h1>Get in touch</h1>
-          <p>We'll make something great together.</p>
+    <main>
+        <div class="contact-page-container">
+            <a href="https://instagram.com/fahdadatin" class="contact-link">
+                <img src="../assets/ui-insta.png" alt="Instagram Link">
+            </a>
+            <a href="https://tumblr.com/fahdadatin" class="contact-link">
+                <img src="../assets/ui-tumblr.png" alt="Tumblr Link">
+            </a>
+            <a href="https://twitter.com/pantherkid" class="contact-link">
+                <img src="../assets/ui-twitter.png" alt="Twitter Link">
+            </a>
+            <a href="#" class="contact-link">
+                <img src="../assets/ui-email.png" alt="Email Address">
+            </a>
         </div>
-      </div>
-      <div class="contact-info contact-container">
-        <div class="contact-flex-container">
-          <div class="contact">
-            <font-awesome-icon icon="envelope" class="contact-icon"/>
-            <h2>E-mail me</h2>
-            <a href="mailto:someone@yoursite.com">dudeski@mail.com</a>
-          </div>
-          <div class="contact">
-            <font-awesome-icon icon="phone" class="contact-icon"/>
-            <h2>Let's talk</h2>
-            <a href="tel:555-555-5555">555-555-5555</a>
-          </div>
-          <div class="contact">
-            <font-awesome-icon :icon="['fab', 'twitter']" class="contact-icon"/>
-            <h2>Tweet me</h2>
-            <a href="https://twitter.com/dudeski">@dudeski</a>
-          </div>
-          <div class="contact">
-            <font-awesome-icon :icon="['fab', 'instagram']" class="contact-icon"/>
-            <h2>Insta me</h2>
-            <a href="https://instagram.com/dudeski">Dudeski</a>
-          </div>
-          <div class="contact">
-            <font-awesome-icon :icon="['fab', 'linkedin']" class="contact-icon"/>
-            <h2>Connect with me</h2>
-            <a href="https://linkedin.com/dudeskiman">Dudeskiman</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+    </main>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
-  }
-};
+    data() {
+        return {}
+    }
+}
 </script>
 
 <style>
+
 .contact-page-container {
-  background-image: linear-gradient(225deg, transparent, #252525 65%),
-    url("../assets/img/photo-bg.jpg");
+    margin-top: 150px;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
 }
-@media screen and (min-width: 900px) {
-  .contact-page-container {
-    background-image: none;
-  }
-  .contact-header:before {
-    background-image: url("../assets/img/photo-bg.jpg");
-  }
+.contact-page-container > a {
+    display: block;
+    margin: 20px 0;
+    text-decoration: none;
+    transition: all .2s ease-in-out;
+    filter: brightness(0.9);
+}
+.contact-page-container > a:hover {
+    transform: scale(1.2);
+    filter: brightness(1);
 }
 </style>
