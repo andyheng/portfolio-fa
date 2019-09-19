16dfa59fbdd6b5598bcd17bd82ba6218b68592b7
Andy <andy.dot.heng@gmail.com>
2019-09-19 13:25:47 -0400

    update transition on app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,62 @@
 <template>
-  <div id="app">
-    <v-header></v-header>
-    <transition name="fade" mode="out-in" @beforeLeave="beforeLeave" @enter="enter" @afterEnter="afterEnter">
-        <keep-alive>
-          <router-view></router-view>
-        </keep-alive>
+  <div id="app" ref="app">
+    <v-header :mobile="mobile"></v-header>
+    <transition
+      name="fade"
+      mode="out-in"
+    >
+      <keep-alive>
+        <router-view :key="$route.fullPath" :mobile="mobile"></router-view>
+      </keep-alive>
     </transition>
     <v-footer></v-footer>
   </div>
 </template>
 
+
+
 <script>
+
+    // <transition
+    //   name="fade"
+    //   mode="out-in"
+    //   @beforeLeave="beforeLeave"
+    //   @enter="enter"
+    //   @afterEnter="afterEnter"
+    // >
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     "v-header": Header,
     "v-footer": Footer
   },
   data() {
     return {
+      mobile: this.setMobile(),
       prevHeight: 0
-    }
+    };
   },
   methods: {
+    setMobile() {
+      const mobileMax = 600;
+      return innerWidth <= mobileMax;
+    },
+    debounced(delay, fn) {
+      let timerId;
+      return function(...args) {
+        if (timerId) {
+          clearTimeout(timerId);
+        }
+        timerId = setTimeout(() => {
+          fn(...args);
+          timerId = null;
+        }, delay);
+      };
+    },
     // before we transition to a new page...
     beforeLeave(e) {
       // getComputedStyle returns an object containing ALL CSS values / properties
@@ -43,8 +74,24 @@ export default {
     afterEnter(e) {
       e.style.height = "auto";
     }
+  },
+  watch: {
+    $route() {
+      if (this.$route.name === "Contact") {
+        this.$refs.app.classList.add("contact-background");
+      } else {
+        this.$refs.app.classList.remove("contact-background");
+      }
+    }
+  },
+  created() {
+    const handleMobile = () => {
+      this.mobile = this.setMobile();
+    }
+    const dHandle = this.debounced(200, handleMobile);
+    window.addEventListener("resize", dHandle);
   }
-}
+};
 </script>
 
 <style lang="sass">
